2d498f960e71e44c89a57c311526a4accc224c12
Anurag Verma <anurag.verma@sla-ubu-393.punesez.searce.local>
2015-07-30 15:20:24 +0530

    solving project euler problem

---
+++ multipleNoSum.py
@@ -0,0 +1,16 @@
+a = input("enter first number\n")
+b= input("choose second number\n")
+limit=[]
+limit.append(a)
+limit.append(b)
+natNo=range(1,10)
+limit=[3,5]
+xyz = []
+for i in limit:
+	xyz +=filter(lambda x: x == i or x % i==0, natNo)	
+set = {}
+map(set.__setitem__, xyz, [])
+nums=set.keys()
+print "the multiples of the given numbers are: "+str(nums)
+c=reduce(lambda x, y:x+y, nums)
+print "the sum of the multiples of the given numbers is "+str(c)
